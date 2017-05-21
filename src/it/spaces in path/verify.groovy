19ed5eb35e322f1011f80267b2b46c1a61736b64
Cosmin Stroe <cosmin.stroe@uptake.com>
2017-05-21 10:12:18 -0500

    Added failing integration test for a project with spaces in the path.

---
+++ src/it/spaces in path/verify.groovy
@@ -0,0 +1,23 @@
+
+//def logs = new File(basedir, "target/build.log")
+def logsFile = new File("build.log")
+
+if (!logsFile.exists()) {
+  throw new Exception("Could not find build.log")
+}
+
+def testSummaryLines = []
+
+logsFile.filterLine { 
+  it ==~ /.*Tests: succeeded [0-9]*, failed [0-9]*, canceled [0-9]*, ignored [0-9]*, pending [0-9]*.*/
+}.each { line -> testSummaryLines << "" + line }
+
+if (testSummaryLines.size == 0) {
+  throw new Exception("Could not find scalatest's summary line in build.log")
+} else if (testSummaryLines.size > 1) {
+  throw new Exception("Found more than one scalatest summary line in build.log")
+}
+
+if (testSummaryLines[0].contains("Tests: succeeded 0, failed 0, canceled 0, ignored 0, pending 0")) {
+  throw new Exception("No tests were run by scalatest!")
+}
